22bc8e92fdf0368ec8e2b825eed2cb2bd51b9af6
Ryan Moriarty <24617283+rmoriar1@users.noreply.github.com>
2021-05-24 22:50:24 +0000

    a

--- build.sh
+++ build.sh
@@ -17,5 +17,5 @@ set -e
 # Install test dependencies
 # Create a custom config file
 pyenv global 3.6.1
-sudo pip install git+https://github.com/ansible-community/ansible-lint.git
-sudo pip install ansible molecule yamllint
+pip install git+https://github.com/ansible-community/ansible-lint.git
+sudo pip333 install ansible molecule yamllint
