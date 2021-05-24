161416db659b92bf066b9295c8feeb9db4ce5cb8
Ryan Moriarty <24617283+rmoriar1@users.noreply.github.com>
2021-05-24 23:14:08 +0000

    a

--- build.sh
+++ build.sh
@@ -3,19 +3,7 @@
 # Fail on any error.
 set -e
 
-# Display commands being run.
-# WARNING: please only enable 'set -x' if necessary for debugging, and be very
-#  careful if you handle credentials (e.g. from Keystore) with 'set -x':
-#  statements like "export VAR=$(cat /tmp/keystore/credentials)" will result in
-#  the credentials being printed in build logs.
-#  Additionally, recursive invocation with credentials as command-line
-#  parameters, will print the full command, with credentials, in the build logs.
-# set -x
-# sudo apt-get update -y
-#pyenv global 3.6.1
-#pip install git+https://github.com/ansible-community/ansible-lint.git
-# Install test dependencies
-# Create a custom config file
+# Install test dependencies.
 pyenv global 3.6.1
 pip3 install git+https://github.com/ansible-community/ansible-lint.git
 sudo pip3 install ansible molecule yamllint
