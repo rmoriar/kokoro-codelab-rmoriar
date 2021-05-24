8478bfe56a0f8c5899971615a6a990e1ad83076a
Ryan Moriarty <24617283+rmoriar1@users.noreply.github.com>
2021-05-24 22:16:54 +0000

    a

--- build.sh
+++ build.sh
@@ -14,7 +14,7 @@ set -e
 sudo apt-get install software-properties-common -y
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt-get update -y
-sudo apt install python3.8 -y
+sudo apt install python3.9 -y
 # Install test dependencies
 sudo pip install --upgrade pip
 sudo pip3 install ansible molecule yamllint "ansible-lint>=4.3.0"
